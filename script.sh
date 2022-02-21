6b8ac0c4a55af7f3e887194666d81946f8e6a5ae
Takieddin <t.fellag@esi-sba.dz>
2022-02-21 23:02:02 +0100

    second commit

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ sudo sh get-docker.sh
 git clone https://github.com/Takieddin/VCL.git
 cd VCL
 sudo docker build -t web-app:latest .
-sudo docker run -d -p 8081:80 web-app
+sudo docker container run -d -p 8081:80 web-app
